e2c1d0c2d462c3cbba53d731bf7e18b4f3b2ee45
Sobieck00 <thomas@sobieck.com>
2015-10-19 09:17:46 -0500

    create new repo script

---
+++ scripts/create_new_repo.ps1
@@ -0,0 +1,38 @@
+Function Add-Item-To-String($string, $item)
+{
+  $newString = $string -join $item
+  return $newString + " "
+}
+
+Function Add-Columns($command)
+{
+  $nameOfColumn = read-host "What is the name of the column?"
+  $type = read-host "What is the type? string (s), integer(i)"
+  $continue = read-host "More Y/N"
+
+  if($type -eq 's')
+  {
+    $type = ":string"
+  }
+
+  $newItemToAdd = $nameOfColumn + $type
+
+  $newString = Add-Item-To-String($command, $newItemToAdd)
+
+  if($continue -eq 'Y')
+  {
+    Add-Columns($newString)
+  }
+  else
+  {
+    Invoke-Expression $newString
+  }
+}
+
+$nameOfObject = read-host "What is the name of your object?"
+$nameOfTable = read-host "What is the name of the table?"
+
+$firstJoin = Add-Item-To-String("mix phoenix.gen.json ", $nameOfObject)
+$secondJoin = Add-Item-To-String($firstJoin, $nameOfTable)
+
+Add-Columns($secondJoin)
