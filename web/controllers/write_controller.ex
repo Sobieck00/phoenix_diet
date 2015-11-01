8e5733504213544988e0154f77e6798a4979047b
Sobieck00 <thomas@sobieck.com>
2015-11-01 06:44:06 -0600

    Testing with comeonin

--- web/controllers/write_controller.ex
+++ web/controllers/write_controller.ex
@@ -2,7 +2,8 @@ defmodule PhoenixDiet.WriteController do
   use PhoenixDiet.Web, :controller
 
   def index(conn, %{"id" => id}) do
-    json conn, %{id: id}
+    hash = Comeonin.create_hash(id)
+    json conn, %{id: hash}
   end
 
   def lisa(conn, %{"is" => is, "lisa" => lisa}) do
